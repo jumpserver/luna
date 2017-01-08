ffb56db498877d25c34a42b29f0f0f77ce6b1b4a
liuzheng712 <liuzheng712@gmail.com>
2017-01-08 14:49:37 +0800

    update fix websocket

--- run_server.py
+++ run_server.py
@@ -3,12 +3,14 @@
 
 
 from config import Config
-from luna import app
-import subprocess
+from luna import app, socket_io
+# import subprocess
 
 app.config.from_object(Config)
 host = app.config['BIND_HOST']
 port = app.config['LISTEN_PORT']
 
 if __name__ == '__main__':
-    subprocess.call('gunicorn -k eventlet -b %s:%s -w 4 -n luna run_server:app' % (host, port), shell=True)
+    # subprocess.call('gunicorn -k eventlet -b %s:%s -w 4 -n luna run_server:app' % (host, port), shell=True)
+    app.bootstrap()
+    socket_io.run(app, host='0.0.0.0')
