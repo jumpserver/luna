9f9f16999afb87b3df472858d30c75496e261f75
liuzheng712 <liuzheng712@gmail.com>
2017-09-02 21:44:13 +0800

    fit for production

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {WelcomeComponent} from './welcome.component';
 import {PageNotFoundComponent} from './not-found.component';
+import {environment} from '../environments/environment';
 
 const appRoutes: Routes = [
   // { path: 'crisis-center', component: CrisisListComponent },
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing: true} // <-- debugging purposes only
+      {enableTracing: !environment.production} // <-- debugging purposes only
     )
   ],
   exports: [
