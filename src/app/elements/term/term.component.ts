585d497190f86c72e917d6741d394e9ad495f7dd
liuzheng712 <liuzheng712@gmail.com>
2018-01-10 15:40:09 +0800

    fix: fix the bug

--- src/app/elements/term/term.component.ts
+++ src/app/elements/term/term.component.ts
@@ -1,7 +1,8 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {ElementRef, Renderer2} from '@angular/core';
+import {ElementRef} from '@angular/core';
 import {term} from '../../globals';
 import {Terminal} from '../../globals';
+import * as jQuery from 'jquery/dist/jquery.min.js';
 
 @Component({
   selector: 'app-element-term',
@@ -12,7 +13,7 @@ export class ElementTermComponent implements OnInit, AfterViewInit {
 
   @ViewChild('term') el: ElementRef;
 
-  constructor(private rd: Renderer2) {
+  constructor() {
   }
 
   ngOnInit() {
@@ -20,30 +21,29 @@ export class ElementTermComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    // term.col = Math.floor(jQuery(this.el.nativeElement).width() / jQuery('#liuzheng').width() * 8) - 3;
-    // term.row = Math.floor(jQuery(this.el.nativeElement).height() / jQuery('#liuzheng').height()) - 5;
-    term.col = 90;
-    term.row = 90;
-    term.term = Terminal({
-      cols: term.col,
-      rows: term.row,
-      useStyle: true,
-      screenKeys: true,
-    });
+    term.col = Math.floor(jQuery(this.el.nativeElement).width() / jQuery('#liuzheng').width() * 8) - 3;
+    term.row = Math.floor(jQuery(this.el.nativeElement).height() / jQuery('#liuzheng').height()) - 5;
+    console.log(term);
+    // term.term = Terminal({
+    //   cols: term.col,
+    //   rows: term.row,
+    //   useStyle: true,
+    //   screenKeys: true,
+    // });
     term.term.open(this.el.nativeElement, true);
-    // const that = this;
-    // window.onresize = function () {
-    //   term.col = Math.floor(jQuery(that.el.nativeElement).width() / jQuery('#liuzheng').width() * 8) - 3;
-    //   term.row = Math.floor(jQuery(that.el.nativeElement).height() / jQuery('#liuzheng').height()) - 5;
-    //
-    //   if (term.col < 80) {
-    //     term.col = 80;
-    //   }
-    //   if (term.row < 24) {
-    //     term.row = 24;
-    //   }
-    //   term.term.resize(term.col, term.row);
-    // };
+    const that = this;
+    window.onresize = function () {
+      term.col = Math.floor(jQuery(that.el.nativeElement).width() / jQuery('#liuzheng').width() * 8) - 3;
+      term.row = Math.floor(jQuery(that.el.nativeElement).height() / jQuery('#liuzheng').height()) - 5;
+
+      if (term.col < 80) {
+        term.col = 80;
+      }
+      if (term.row < 24) {
+        term.row = 24;
+      }
+      term.term.resize(term.col, term.row);
+    };
   }
 
 }
