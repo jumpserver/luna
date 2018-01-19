b4f28ca778e957a6d8783bc258b6869020b0168b
liuzheng712 <liuzheng712@gmail.com>
2018-01-19 16:42:01 +0800

    feat: update

--- src/app/elements/guacamole/guacamole.component.ts
+++ src/app/elements/guacamole/guacamole.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {NavList} from '../../ControlPage/control/control.component';
 
 @Component({
   selector: 'app-element-guacamole',
@@ -6,13 +8,24 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./guacamole.component.scss']
 })
 export class ElementGuacamoleComponent implements OnInit {
-  @Input() target: string;
+  @Input() host: any;
+  @Input() userid: any;
+  @Input() index: number;
+  target: string;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
-    this.target = '/guacamole/?asset_id=' + '&system_user_id=' + '';
+    // this.target = '/guacamole/?asset_id=' + '&system_user_id=' + '';
+    this.target = 'https://www.baidu.com';
   }
 
+  trust(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  Disconnect() {
+    NavList.List[this.index].connected = false;
+  }
 }
