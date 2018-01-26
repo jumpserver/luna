c6dc9f2e467a9c17152e7adb0ff101082da701d9
liuzheng712 <liuzheng712@gmail.com>
2018-01-26 08:16:57 +0800

    fix: replace term.js to xterm

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -71,7 +71,7 @@ export class HttpService {
 
   get_guacamole_token(username: string, assetID: string, systemUserID: string) {
     return this.get('/guacamole/api/tokens?username=' + username + '&password=zheng&asset_id=' +
-      assetID + '&system_user_id=' + systemUserID, {headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')});
+      assetID + '&system_user_id=' + systemUserID, {headers: new HttpHeaders().set('Content-type', 'application/x-www-form-urlencoded')});
   }
 
   search(q: string) {
