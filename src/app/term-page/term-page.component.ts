d9cb7e13fce127e9b7ceb5f64dda87dba2fbc3e2
liuzheng712 <liuzheng712@gmail.com>
2018-01-31 22:05:20 +0800

    feat: update

--- src/app/term-page/term-page.component.ts
+++ src/app/term-page/term-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataStore} from '../globals';
 
 @Component({
   selector: 'app-term-page',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    DataStore.NavShow = false;
+  }
 
   ngOnInit() {
   }
