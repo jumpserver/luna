2e1bfb6d976a2e9a82f70d9a6e41c95f8f77fc8c
liuzheng712 <liuzheng712@gmail.com>
2018-02-01 18:39:55 +0800

    feat: term token page

--- src/app/term-page/term-page.component.ts
+++ src/app/term-page/term-page.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataStore} from '../globals';
+import {ActivatedRoute, Params} from '@angular/router';
+import * as jQuery from 'jquery/dist/jquery.min.js';
 
 @Component({
   selector: 'app-term-page',
@@ -7,12 +9,16 @@ import {DataStore} from '../globals';
   styleUrls: ['./term-page.component.scss']
 })
 export class TermPageComponent implements OnInit {
+  token: string;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute) {
     DataStore.NavShow = false;
   }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.token = params['token'];
+    });
+    jQuery('body').css('background-color', 'black');
   }
-
 }
