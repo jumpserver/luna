ad077e236c5b41b8e8990694512ddefef38f4c32
liuzheng712 <liuzheng712@gmail.com>
2018-02-08 19:36:12 +0800

    fix: update

--- src/app/elements/leftbar/leftbar.component.ts
+++ src/app/elements/leftbar/leftbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-element-leftbar',
@@ -6,8 +6,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leftbar.component.scss']
 })
 export class ElementLeftbarComponent implements OnInit {
+  leftbar = [
+    {
+      'name': 'Dashboard',
+      'class': 'fa fa-dashboard',
+      'label': 'label label-info pull-right'
+    },
+    {
+      'name': 'Users',
+      'class': 'fa fa-group',
+      'label': 'fa arrow',
+      'child': [
+        {
+          'name': 'User',
+        }, {
+          'name': 'User group',
+        }, {
+          'name': 'Login logs',
+        }
+      ]
+    },
+    {
+      'name': 'Assets',
+      'class': 'fa fa-inbox',
+      'label': 'fa arrow',
+      'child': [
+        {
+          'name': 'Asset',
+        }, {
+          'name': 'Asset group',
+        }, {
+          'name': 'Cluster',
+        }, {
+          'name': 'Admin user',
+        }, {
+          'name': 'System user',
+        }, {
+          'name': 'Labels',
+        }
+      ]
+    },
+    {
+      'name': 'Perms',
+      'class': 'fa fa-edit',
+      'label': 'fa arrow',
+      'child': [{'name': 'Asset permission'}]
+    },
+    {
+      'name': 'Sessions',
+      'class': 'fa fa-rocket',
+      'label': 'fa arrow',
+      'child': [
+        {
+          'name': 'Session online'
+        },
+        {
+          'name': 'Session offline'
+        },
+        {
+          'name': 'Commands'
+        },
+        {
+          'name': 'Terminal'
+        },
+      ]
+    },
+    {
+      'name': 'Job Center',
+      'class': 'fa fa-coffee',
+      'label': 'fa arrow',
+      'child': [
+        {
+          'name': 'Task'
+        },
+      ]
+    },
+    {
+      'name': 'Settings',
+      'class': 'fa fa-gears',
+      'label': 'label label-info pull-right'
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
