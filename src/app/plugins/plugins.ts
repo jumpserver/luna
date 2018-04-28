ffbbb518e4dc90623d7d62042db612ec52e1a092
liuzheng712 <liuzheng712@gmail.com>
2018-04-28 11:39:12 +0800

    fix: update

--- src/app/plugins/plugins.ts
+++ src/app/plugins/plugins.ts
@@ -1,8 +1,16 @@
 import {MaterialModule} from './MaterialModule.component';
 import {NgxUIModule, SplitModule} from '@swimlane/ngx-ui';
+import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgProgressModule} from 'ngx-progressbar';
 
 export const PluginModules = [
+  BrowserAnimationsModule,
+  NgProgressModule,
   MaterialModule,
+  LoggerModule.forRoot({serverLoggingUrl: '/api/logs', level: NgxLoggerLevel.DEBUG, serverLogLevel: NgxLoggerLevel.ERROR}),
+  NgxDatatableModule,
   NgxUIModule,
   SplitModule
 ];
