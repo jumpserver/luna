9be4982313d55075328793418d239c8ba81ab02c
ibuler <ibuler@qq.com>
2018-05-28 16:01:42 +0800

    [Update] 修改显示标题过长的问题

---
+++ src/app/pipes/truncatechars.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({name: 'truncatechars'})
+export class TruncatecharsPipe implements PipeTransform {
+  transform(value: string, length: number): string {
+    if (!value) {
+      return value;
+    }
+    if (value.length < length) {
+      return value;
+    }
+    return value.slice(0, length) + '..';
+  }
+}
