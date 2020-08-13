53d322712bb3e6f7effbc7264a45e46e56343ee3
OrangeM21 <orangemtony@gmail.com>
2020-08-13 15:13:32 +0800

    fix: 修正新窗口打开的应用的问题

--- src/app/pages/connect/connect.component.ts
+++ src/app/pages/connect/connect.component.ts
@@ -32,6 +32,8 @@ export class PagesConnectComponent implements OnInit {
     const assetId = this._appService.getQueryString('asset');
     const shareroomId = this._appService.getQueryString('shareroom');
     const remoteAppId = this._appService.getQueryString('remote_app');
+    const databaseAppId = this._appService.getQueryString('database_app');
+    const k8sId = this._appService.getQueryString('k8s_app');
     if (this.system && this.token) {
       const evt = new ConnectEvt(new TreeNode(), 'token');
       connectEvt.next(evt);
@@ -67,5 +69,28 @@ export class PagesConnectComponent implements OnInit {
         }
       );
     }
+    if (databaseAppId) {
+      this._http.getMyGrantedDBApps(databaseAppId).subscribe(
+        nodes => {
+          if (!nodes) {
+            return;
+          }
+          const evt = new ConnectEvt(nodes[0], 'asset');
+          connectEvt.next(evt);
+        }
+      );
+    }
+    if (k8sId) {
+      this._http.getMyGrantedK8SApps(k8sId).subscribe(
+        nodes => {
+          if (!nodes) {
+            return;
+          }
+          const evt = new ConnectEvt(nodes[0], 'asset');
+          connectEvt.next(evt);
+        }
+      );
+    }
+
   }
 }
