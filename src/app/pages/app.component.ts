731fe4351ac1dcea27b6241ca306d04fe4a77973
老广 <ibuler@qq.com>
2020-08-20 01:32:48 -0500

    chore: merge dev to master (#244)
    * fix: 修复RemoteApp取不到Title的问题
    
    * fix: 默认语言改成中文
    
    Co-authored-by: OrangeM21 <orangemtony@gmail.com>

--- src/app/pages/app.component.ts
+++ src/app/pages/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   // tslint:disable-next-line:use-life-cycle-interface
   public async ngOnInit() {
     // 语言初始化(若未设置语言, 则取浏览器语言)
-    const currentLanguage = await localStorage.getItem('currentLanguage') || this.translate.getBrowserCultureLang();
+    const currentLanguage = await localStorage.getItem('currentLanguage') || 'zh';
     // 当在assets/i18n中找不到对应的语言翻译时，使用'zh-CN'作为默认语言
     this.translate.setDefaultLang('zh');
     this.translate.use(currentLanguage);
