79e5f89f099ee722e8c7757213bb8688adad14f5
ibuler <ibuler@qq.com>
2021-07-27 15:54:34 +0800

    stash

---
+++ src/app/elements/connect/connect-dialog/manual-auth/manual-auth.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'elements-manual-auth',
+  templateUrl: 'elements-manual-auth.component.html',
+  styleUrls: ['./elements-manual-auth.component.scss'],
+})
+export class ManualAuthComponent implements  OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+}
