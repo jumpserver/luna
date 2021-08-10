da383b0f2907391be14562cd5f4e8f8d449b4c6b
Eric <xplzv@126.com>
2021-08-10 10:02:43 +0800

    fix: 修复 remote app 连接问题

--- src/app/elements/content-window/lion/lion.component.ts
+++ src/app/elements/content-window/lion/lion.component.ts
@@ -47,7 +47,7 @@ export class ElementConnectorLionComponent implements OnInit {
   }
 
   generateNodeURL() {
-    if (this.view.type === 'remote_app') {
+    if (this.view.type === 'remote_app' || this.view.type === 'application' ) {
       this.iframeURL = `${this.baseUrl}/?target_id=${this.node.id}&type=remoteapp&system_user_id=${this.sysUser.id}`;
     } else {
       this.iframeURL = `${this.baseUrl}/?target_id=${this.node.id}&type=${this.protocol}&system_user_id=${this.sysUser.id}`;
