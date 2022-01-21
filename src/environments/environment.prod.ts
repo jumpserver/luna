c835ec0fbe83951db9a6d992b88495a71658a556
“怀磊” <2280131253@qq.com>
2022-01-21 18:51:58 +0800

    feat: 不显示开源协议版本号

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true
 };
-// export const version = '1.3.0-{{BUILD_NUMBER}} GPLv2.';
-export const version = '2.0.2-101 GPLv2.';
+export const version = '2.0.2';
 // export const version = '1.4.1-{{BUILD_NUMBER}} GPLv2.';
