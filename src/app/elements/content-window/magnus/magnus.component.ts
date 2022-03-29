0c8fdba52450af101d82445e10c216ffa4501d80
fit2bot <68588906+fit2bot@users.noreply.github.com>
2022-03-29 12:59:18 +0800

    perf: 优化 magnus terminal (#521)
    * perf: 优化 magnus terminal
    
    * perf: 优化复制粘贴
    
    * perf: 修改 eye
    
    * perf: 修改翻译
    
    Co-authored-by: ibuler <ibuler@qq.com>

--- src/app/elements/content-window/magnus/magnus.component.ts
+++ src/app/elements/content-window/magnus/magnus.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit, ViewChild, ElementRef, Inject} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {View, SystemUser, TreeNode} from '@app/model';
-import {HttpService, LogService, SettingService} from '@app/services';
+import {HttpService, I18nService, LogService, SettingService} from '@app/services';
+import {User} from '@app/globals';
+import {ToastrService} from 'ngx-toastr';
 
 
 @Component({
@@ -10,55 +12,113 @@ import {HttpService, LogService, SettingService} from '@app/services';
 })
 export class ElementConnectorMagnusComponent implements OnInit {
   @Input() view: View;
-  @ViewChild('terminal', {static: false}) iframe: ElementRef;
 
   iframeURL: any;
   node: TreeNode;
   sysUser: SystemUser;
   protocol: string;
-  username: string;
-  password: string;
-  host: string;
-  port: number = 33060;
   cli: string;
+  cliSafe: string;
   protocolPorts: object;
+  dbInfoItems: Array<any>;
+  dbInfo: any;
+  globalSetting: any;
+  loading = true;
+  passwordDark = '******';
+  passwordShow = '******';
 
   constructor(private _logger: LogService,
               private _http: HttpService,
+              private _i18n: I18nService,
+              private _toastr: ToastrService,
               private _settingSvc: SettingService
   ) {
+    this.dbInfoItems = [
+      {name: 'host', label: this._i18n.t('Host')},
+      {name: 'port',  label: this._i18n.t('Port')},
+      {name: 'username', label: this._i18n.t('Username')},
+      {name: 'password',  label: this._i18n.t('Password')},
+      {name: 'database', label: this._i18n.t('Database')},
+      {name: 'protocol', label: this._i18n.t('Protocol')},
+    ];
+    this.dbInfo = {username: '', password: '', host: '', port: '', database: '', protocol: ''};
+    this.globalSetting = this._settingSvc.globalSetting;
   }
 
   ngOnInit() {
     const {node, sysUser, protocol} = this.view;
-    this.host = this._settingSvc.globalSetting.TERMINAL_MAGNUS_HOST;
+    this.dbInfo.host = this.globalSetting.TERMINAL_MAGNUS_HOST;
     this.protocolPorts = {
-      mysql: this._settingSvc.globalSetting.TERMINAL_MAGNUS_MYSQL_PORT,
-      postgresql: this._settingSvc.globalSetting.TERMINAL_MAGNUS_POSTGRE_PORT
+      mysql: this.globalSetting.TERMINAL_MAGNUS_MYSQL_PORT,
+      postgresql: this.globalSetting.TERMINAL_MAGNUS_POSTGRE_PORT
     };
     this.node = node;
     this.sysUser = sysUser;
-    this.protocol = protocol;
-    this.port = this.protocolPorts[protocol];
+    this.dbInfo.protocol = protocol;
+    this.dbInfo.port = this.protocolPorts[protocol];
+    this.dbInfo.database = node.meta.data.attrs.database;
     this.generateConnectCLI();
   }
 
   generateConnectCLI() {
     this._http.getConnectionToken(this.sysUser.id, '', this.node.id).then((token) => {
-      this.username = token.id;
-      this.password = token.secret;
+      this.dbInfo.username = token.id;
+      this.dbInfo.password = token.secret;
+      // 密码占位，因为有 safe cli, 需要把密码隐藏，所以占位替换
+      const passwordHolder = `@${this.dbInfo.password}@`;
+      let cli = '';
 
-      switch (this.protocol) {
+      switch (this.dbInfo.protocol) {
         case 'mysql':
         case 'mariadb':
-          this.cli = `$ mysql -u${token.id} -p${token.secret} -hjumpserver-test.fit2cloud.com -P${this.port}`;
+          cli = `mysql
+            -u ${token.id}
+            -p${passwordHolder}
+            -h ${this.dbInfo.host}
+            -P ${this.dbInfo.port}
+            ${this.dbInfo.database}
+          `;
           break;
         case 'postgresql':
-          this.cli = `$ psql -U ${token.id} -h jumpserver-test.fit2cloud.com -p ${this.port}\n Password: ${token.secret}`;
+          cli = `PGPASSWORD=${passwordHolder} psql
+            -U ${token.id}
+            -h ${this.dbInfo.host}
+            -d ${this.dbInfo.database}
+            -p ${this.dbInfo.port}
+          `;
           break;
         default:
-          this.cli = `Protocol '${this.protocol}' Not support now`;
+          cli = `Protocol '${this.dbInfo.protocol}' Not support now`;
       }
+      this.cliSafe = cli.replace(passwordHolder, this.passwordDark);
+      this.cli = cli.replace(passwordHolder, this.dbInfo.password);
+      this.loading = false;
     });
+  }
+
+  startClient() {
+    const data = {
+      protocol: this.dbInfo.protocol,
+      username: User.username,
+      command: this.cli
+    };
+    const json = JSON.stringify(data);
+    const b64 = window.btoa(json);
+    const url = 'jms://' + b64;
+    window.open(url);
+  }
+
+  showPassword($event) {
+    $event.stopPropagation();
+    if (this.passwordShow === this.passwordDark) {
+      this.passwordShow = this.dbInfo.password;
+    } else {
+      this.passwordShow = this.passwordDark;
+    }
+  }
+
+  async onCopySuccess(evt) {
+    const msg = await this._i18n.t('Copied');
+    this._toastr.success(msg);
   }
 }
--- src/app/plugins/plugins.ts
+++ src/app/plugins/plugins.ts
@@ -1,9 +1,9 @@
 import {MaterialModule} from './MaterialModule.component';
-// import {SplitModule, NgxUIModule} from '@swimlane/ngx-ui';
 import {SplitModule} from '@app/plugins/split/split.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ClipboardModule} from 'ngx-clipboard';
 
 
 export const PluginModules = [
@@ -12,7 +12,7 @@ export const PluginModules = [
   LoggerModule.forRoot({level: NgxLoggerLevel.DEBUG, serverLogLevel: NgxLoggerLevel.ERROR}),
   FlexLayoutModule,
   SplitModule,
-  // NgxUIModule,
+  ClipboardModule
 ];
 
 
