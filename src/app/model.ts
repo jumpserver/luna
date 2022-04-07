6652c89c6ccd098dda3a407a6f8bd3b2d2e49e1e
ibuler <ibuler@qq.com>
2022-04-07 19:36:53 +0800

    perf: 修改变量

--- src/app/model.ts
+++ src/app/model.ts
@@ -230,7 +230,7 @@ export class Setting {
   rdpFullScreen: number = 1;
   rdpDrivesRedirect: number = 0;
   fontSize: number = 14;
-  backspaceAsCrtlH: string = '0';
+  backspaceAsCtrlH: string = '0';
   isLoadTreeAsync: string = '1';
   isSkipAllManualPassword: string = '0';
   quickPaste = '0';
