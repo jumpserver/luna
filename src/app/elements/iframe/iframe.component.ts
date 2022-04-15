ee9bf44704c78662e4e6bce50b4a5825b8b6860c
Jiangjie.Bai <bugatti_it@163.com>
2022-04-15 14:26:43 +0800

    fix: 修改postmessage长时间未收到响应时主动关闭监听和发送

--- src/app/elements/iframe/iframe.component.ts
+++ src/app/elements/iframe/iframe.component.ts
@@ -68,6 +68,15 @@ export class ElementIframeComponent implements OnInit, AfterViewInit, OnDestroy 
       this.iframeWindow.postMessage({name: 'PING', id: this.id}, '*');
     }, 500);
     window.addEventListener('message', this.eventHandler);
+
+    setTimeout(function () {
+      if ( this.show === true) {
+        return;
+      }
+      // 长时间未PING通, 则主动关闭
+      clearInterval(this.ping);
+      window.removeEventListener('message', this.eventHandler);
+    }.bind(this), 500 * 20);
   }
 
   sendCommand(data) {
