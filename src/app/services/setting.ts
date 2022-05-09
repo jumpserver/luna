ecd1f50e3411e7e3a88c58ff0078a09d71bac332
Jiangjie.Bai <bugatti_it@163.com>
2022-05-09 16:58:33 +0800

    fix: 修改settings API直接返回配置项，不再包含 data 作为 key

--- src/app/services/setting.ts
+++ src/app/services/setting.ts
@@ -27,25 +27,25 @@ export class SettingService {
   }
   async getPublicSettings() {
     const resp = await this._http.get<any>('/api/v1/settings/public/').toPromise();
-    this.globalSetting = resp['data'];
+    this.globalSetting = resp;
     this.setting.commandExecution = this.globalSetting.SECURITY_COMMAND_EXECUTION;
 
     // 更改favicon
     const link: any = document.querySelector('link[rel*=\'icon\']') || document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = resp['data'].LOGO_URLS.favicon;
+    link.href = resp.LOGO_URLS.favicon;
 
     // 改logo
     const logoRef: any = document.getElementById('left-logo');
 
     // 统一修改，避免生效速度不一致
     document.getElementsByTagName('head')[0].appendChild(link);
-    const logoLogout = resp['data'].LOGO_URLS.logo_logout;
+    const logoLogout = resp.LOGO_URLS.logo_logout;
     if (logoLogout && logoRef) {
       logoRef.src = logoLogout;
     }
-    document.title = this._i18n.instant('Web Terminal') + ` - ${resp.data.LOGIN_TITLE}`;
+    document.title = this._i18n.instant('Web Terminal') + ` - ${resp.LOGIN_TITLE}`;
     return new Promise((resolve) => { resolve(true); });
   }
 
