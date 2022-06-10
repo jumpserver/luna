2086cf6ddd58c639ba0f6dbe3d48f2232faff975
ibuler <ibuler@qq.com>
2022-06-10 18:50:35 +0800

    perf: 修改 cookie

--- src/app/services/organization.ts
+++ src/app/services/organization.ts
@@ -11,7 +11,7 @@ export class OrganizationService {
   constructor(private _cookie: CookieService) {}
 
   public switchOrg(org): void {
-    this._cookie.set('X-JMS-ORG', org.id, 3600, '/', document.domain, true, 'Lax');
+    this._cookie.set('X-JMS-ORG', org.id, 30);
     this.currentOrgChange$.emit();
   }
 
