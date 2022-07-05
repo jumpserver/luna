12cace8a9e07fabcfe98eb502db9b16d0adb26d2
fit2bot <68588906+fit2bot@users.noreply.github.com>
2022-07-05 14:39:54 +0800

    perf: 优化主题 (#636)
    * perf: 优化主题
    
    * perf: 修改 css
    
    * perf: 修改 css
    
    Co-authored-by: ibuler <ibuler@qq.com>

--- src/app/services/setting.ts
+++ src/app/services/setting.ts
@@ -26,28 +26,43 @@ export class SettingService {
     this.init().then();
   }
   async getPublicSettings() {
-    const resp = await this._http.get<any>('/api/v1/settings/public/').toPromise();
-    this.globalSetting = resp;
+    this.globalSetting = await this._http.get<any>('/api/v1/settings/public/').toPromise();
     this.setting.commandExecution = this.globalSetting.SECURITY_COMMAND_EXECUTION;
 
-    // 更改favicon
-    const link: any = document.querySelector('link[rel*=\'icon\']') ||
-      document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = resp.LOGO_URLS.favicon;
+    this.setLogo();
+    this.setTitle();
+    this.setFavicon();
+    this.setPrimaryColor();
+    return new Promise((resolve) => { resolve(true); });
+  }
 
-    // 改logo
+  setTitle() {
+    document.title = this._i18n.instant('Web Terminal') + ` - ${this.globalSetting.INTERFACE.login_title}`;
+  }
+
+  setLogo() {
     const logoRef: any = document.getElementById('left-logo');
-
-    // 统一修改，避免生效速度不一致
-    document.getElementsByTagName('head')[0].appendChild(link);
-    const logoLogout = resp.LOGO_URLS.logo_logout;
+    const logoLogout = this.globalSetting.INTERFACE.logo_logout;
     if (logoLogout && logoRef) {
       logoRef.src = logoLogout;
     }
-    document.title = this._i18n.instant('Web Terminal') + ` - ${resp.LOGIN_TITLE}`;
-    return new Promise((resolve) => { resolve(true); });
+  }
+
+  setFavicon() {
+    // 更改favicon
+    const link: any = document.querySelector('link[rel*=\'icon\']') ||
+      document.createElement('link');
+    document.getElementsByTagName('head')[0].appendChild(link);
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = this.globalSetting.INTERFACE.favicon;
+  }
+
+  setPrimaryColor() {
+    const primaryColor = this.globalSetting.INTERFACE['primary_color'];
+    if (primaryColor) {
+      document.body.style.setProperty('--primary-color', primaryColor);
+    }
   }
 
   async init() {
