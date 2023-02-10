e3c3ee422a60fc95e23aeb241babcd4780af2142
“huailei000” <2280131253@qq.com>
2023-02-10 12:42:39 +0800

    fix: 修复全屏状态下ESC不能退出问题

--- src/app/pages/app.component.ts
+++ src/app/pages/app.component.ts
@@ -1,14 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {I18nService} from '@app/services';
-
+import { EventManager } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent {
-  constructor(_i18n: I18nService) {
+export class AppComponent implements OnInit {
+  constructor(_i18n: I18nService,
+              private eventManager: EventManager
+  ) {}
+  ngOnInit(): void {
+    this.eventManager.addGlobalEventListener('window', 'keyup.esc', () => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+      }
+    });
   }
 }
 
