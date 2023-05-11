e48727896d9d05c7f1f2c69e5ec6f35ff2b344a5
halo <wuyihuangw@gmail.com>
2023-05-11 18:21:58 +0800

    feat: 支持url传参设置当前组织

--- src/app/services/app.ts
+++ src/app/services/app.ts
@@ -20,6 +20,7 @@ import {AuthInfo, ConnectData, SystemUser, TreeNode, Endpoint, Protocol, View} f
 import * as CryptoJS from 'crypto-js';
 import {getCookie, setCookie} from '@app/utils/common';
 import {OrganizationService} from './organization';
+import {Organization} from '@app/model';
 
 declare function unescape(s: string): string;
 
@@ -46,6 +47,7 @@ export class AppService {
               private _localStorage: LocalStorageService,
               private _orgSvc: OrganizationService) {
     this.setLogLevel();
+    this.setOrgFromQueryString();
     this.checkLogin();
     this.loadPreferData();
     this.loadOriManualAuthInfo();
@@ -58,6 +60,14 @@ export class AppService {
         logLevel = environment.production ? '1' : '5';
     }
     this._logger.level = parseInt(logLevel, 10);
+  }
+
+  setOrgFromQueryString() {
+    const oid = this.getQueryString('oid');
+    if (oid) {
+      const currentOrg: Organization = {id: oid, name: ''};
+      this._orgSvc.switchOrg(currentOrg);
+    }
   }
 
   checkLogin() {
