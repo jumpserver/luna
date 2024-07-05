3214b3ae7b95936a322cfcb7dd544f50bdad2a00
zhaojisen <1301338853@qq.com>
2024-07-05 14:03:35 +0800

    docs: modify the README file

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -43,27 +43,27 @@ module.exports = {
 	},
 	prompt: {
 		messages: {
-			type: "Select the type of change that you're committing:",
-			scope: "Denote the SCOPE of this change (optional):",
-			customScope: "Denote the SCOPE of this change:",
-			subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
-			body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-			breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
-			footerPrefixsSelect: "Select the ISSUES type of changeList by this change (optional):",
-			customFooterPrefixs: "Input ISSUES prefix:",
-			footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
-			confirmCommit: "Are you sure you want to proceed with the commit above?"
+			// type: "Select the type of change that you're committing:",
+			// scope: "Denote the SCOPE of this change (optional):",
+			// customScope: "Denote the SCOPE of this change:",
+			// subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+			// body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+			// breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
+			// footerPrefixsSelect: "Select the ISSUES type of changeList by this change (optional):",
+			// customFooterPrefixs: "Input ISSUES prefix:",
+			// footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
+			// confirmCommit: "Are you sure you want to proceed with the commit above?"
 			// 中文版
-			// type: "选择你要提交的类型 :",
-			// scope: "选择一个提交范围（可选）:",
-			// customScope: "请输入自定义的提交范围 :",
-			// subject: "填写简短精炼的变更描述 :\n",
-			// body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-			// breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-			// footerPrefixsSelect: "选择关联issue前缀（可选）:",
-			// customFooterPrefixs: "输入自定义issue前缀 :",
-			// footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
-			// confirmCommit: "是否提交或修改commit ?"
+			type: "选择你要提交的类型 :",
+			scope: "选择一个提交范围（可选）:",
+			customScope: "请输入自定义的提交范围 :",
+			subject: "填写简短精炼的变更描述 :\n",
+			body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+			breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+			footerPrefixsSelect: "选择关联issue前缀（可选）:",
+			customFooterPrefixs: "输入自定义issue前缀 :",
+			footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
+			confirmCommit: "是否提交或修改commit ?"
 		},
 		types: [
 			{
