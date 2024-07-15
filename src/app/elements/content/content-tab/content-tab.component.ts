caa506c969ebc7871d9e18654f975553ee9f038c
zhaojisen <1301338853@qq.com>
2024-07-15 17:57:29 +0800

    fixed: Fixed tab renaming issue

--- src/app/elements/content/content-tab/content-tab.component.ts
+++ src/app/elements/content/content-tab/content-tab.component.ts
@@ -1,31 +1,74 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {View, ViewAction} from '@app/model';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { View, ViewAction } from "@app/model";
 
 @Component({
-  selector: 'elements-content-tab',
-  templateUrl: './content-tab.component.html',
-  styleUrls: ['./content-tab.component.scss'],
+  selector: "elements-content-tab",
+  templateUrl: "./content-tab.component.html",
+  styleUrls: ["./content-tab.component.scss"],
 })
 export class ElementContentTabComponent implements OnInit {
   @Input() view: View;
   @Output() onAction: EventEmitter<ViewAction> = new EventEmitter<ViewAction>();
+  @ViewChild("inputElement") inputElement: ElementRef;
+
   public iconCls: string;
+  private shouldFocusInput = false;
+  private clickTimeout: any;
 
   ngOnInit(): void {
     if (!this.view.asset) {
-      this.iconCls = 'fa-linux';
+      this.iconCls = "fa-linux";
     } else {
-      this.iconCls = 'fa-' + this.view.asset.type.value;
+      this.iconCls = "fa-" + this.view.asset.type.value;
+    }
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.shouldFocusInput && this.inputElement) {
+      this.inputElement.nativeElement.focus();
+      this.shouldFocusInput = false;
     }
   }
 
   close() {
-    const action = new ViewAction(this.view, 'close');
+    const action = new ViewAction(this.view, "close");
     this.onAction.emit(action);
   }
 
   setActive() {
-    const action = new ViewAction(this.view, 'active');
-    this.onAction.emit(action);
+    if (this.clickTimeout) {
+      clearTimeout(this.clickTimeout);
+      this.clickTimeout = null;
+    }
+
+    // 延迟300毫秒，检查是否有双击事件
+    this.clickTimeout = setTimeout(() => {
+      const action = new ViewAction(this.view, "active");
+      this.onAction.emit(action);
+      this.clickTimeout = null;
+    }, 300);
+  }
+
+  onDoubleClick() {
+    if (this.clickTimeout) {
+      clearTimeout(this.clickTimeout);
+      this.clickTimeout = null;
+    }
+    this.view.editable = true;
+    this.shouldFocusInput = true;
+  }
+
+  onBlur() {
+    setTimeout(() => {
+      this.view.editable = false;
+    }, 200);
   }
 }
