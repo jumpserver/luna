b4701fdf223ba12f46fc73b009b4bf3a019a32e5
fit2bot <robot@jumpserver.org>
2024-08-21 15:45:09 +0800

    feat: Update v3.10.13

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true
 };
-export const version = '2.0.2';
-// export const version = '1.4.1-{{BUILD_NUMBER}} GPLv2.';
+export const version = 'v3.10.13';
+// export const version = 'v3.10.13';
