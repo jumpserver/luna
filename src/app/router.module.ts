ca4dc94f12632121f39d5668ec824244690e64f4
ibuler <ibuler@qq.com>
2025-04-07 10:12:33 +0800

    perf: direct connect

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { PageMainComponent } from "./pages/main/main.component";
-import { PageSftpComponent } from "./pages/sftp/sftp.component";
-import { PagesBlankComponent } from "./pages/blank/blank.component";
-import { PageDirectComponent } from "./pages/direct/direct.component";
-import { PagesReplayComponent } from "./pages/replay/replay.component";
-import { PagesConnectComponent } from "./pages/connect/connect.component";
-import { PagesMonitorComponent } from "./pages/monitor/monitor.component";
-import { PagesKubernetesComponent } from "./pages/Kubernetes/kubernetes.component";
+import { PageMainComponent } from './pages/main/main.component';
+import { PageSftpComponent } from './pages/sftp/sftp.component';
+import { PagesBlankComponent } from './pages/blank/blank.component';
+import { PageDirectComponent } from './pages/direct/direct.component';
+import { PagesReplayComponent } from './pages/replay/replay.component';
+import { PagesConnectComponent } from './pages/connect/connect.component';
+import { PagesMonitorComponent } from './pages/monitor/monitor.component';
+import { PagesKubernetesComponent } from './pages/Kubernetes/kubernetes.component';
 
 const appRoutes: Routes = [
-  { path: "", component: PageMainComponent },
-  { path: "sftp", component: PageSftpComponent },
-  { path: "connect", component: PagesConnectComponent },
-  { path: "undefined", component: PagesBlankComponent },
-  { path: "replay/:sid", component: PagesReplayComponent },
-  { path: "monitor/:sid", component: PagesMonitorComponent },
-  { path: "k8s/:token", component: PagesKubernetesComponent },
-  { path: "direct_connect/:userId/:username/:assetId/:assetName/:protocol", component: PageDirectComponent },
+  { path: '', component: PageMainComponent },
+  { path: 'sftp', component: PageSftpComponent },
+  { path: 'connect', component: PagesConnectComponent },
+  { path: 'undefined', component: PagesBlankComponent },
+  { path: 'replay/:sid', component: PagesReplayComponent },
+  { path: 'monitor/:sid', component: PagesMonitorComponent },
+  { path: 'k8s/:token', component: PagesKubernetesComponent },
+  { path: 'admin-connect', component: PageDirectComponent },
   // { path: '**', component: PagesNotFoundComponent }
 ];
 
