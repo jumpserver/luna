16acef2dbada6a6141c719dc6e6aad74ea29fa89
zhaojisen <1301338853@qq.com>
2025-04-15 17:12:32 +0800

    Fixed: Fix the communication issues between k8s and regular session sharing with Luna

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageMainComponent } from './pages/main/main.component';
 import { PageSftpComponent } from './pages/sftp/sftp.component';
 import { PagesBlankComponent } from './pages/blank/blank.component';
+import { PagesShareComponent  } from './pages/share/share.component';
 import { PageDirectComponent } from './pages/direct/direct.component';
 import { PagesReplayComponent } from './pages/replay/replay.component';
 import { PagesConnectComponent } from './pages/connect/connect.component';
@@ -15,10 +16,11 @@ const appRoutes: Routes = [
   { path: 'sftp', component: PageSftpComponent },
   { path: 'connect', component: PagesConnectComponent },
   { path: 'undefined', component: PagesBlankComponent },
+  { path: 'share/:id', component: PagesShareComponent },
   { path: 'replay/:sid', component: PagesReplayComponent },
+  { path: 'admin-connect', component: PageDirectComponent },
   { path: 'monitor/:sid', component: PagesMonitorComponent },
   { path: 'k8s/:token', component: PagesKubernetesComponent },
-  { path: 'admin-connect', component: PageDirectComponent },
   // { path: '**', component: PagesNotFoundComponent }
 ];
 
