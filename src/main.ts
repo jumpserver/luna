1b036194af31190474042bad59f5eec3b5356f74
ibuler <ibuler@qq.com>
2025-05-28 17:49:54 +0800

    stash it

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import 'codemirror/mode/javascript/javascript';
 import {AppModule} from '@app/app.module';
 import {environment} from './environments/environment';
 
