79e59965b4501bdc3eddc19528a4dcf931141461
ibuler <ibuler@qq.com>
2025-06-04 10:09:04 +0800

    perf: 加快构建速度

--- src/app/plugins/ZorroModule.component.ts
+++ src/app/plugins/ZorroModule.component.ts
@@ -1,22 +1,23 @@
-import {NgModule} from '@angular/core';
-import {NzButtonModule} from 'ng-zorro-antd/button';
-import {NzModalModule} from 'ng-zorro-antd/modal';
-import {NzIconModule} from 'ng-zorro-antd/icon';
-import {NzInputModule} from 'ng-zorro-antd/input';
-import {NzSelectModule} from 'ng-zorro-antd/select';
-import {NzFormModule} from 'ng-zorro-antd/form';
-import {NzAlertModule} from 'ng-zorro-antd/alert';
-import {NzCheckboxModule} from 'ng-zorro-antd/checkbox';
-import {NzCardModule} from 'ng-zorro-antd/card';
-import {NzSwitchModule} from 'ng-zorro-antd/switch';
-import {NzDropDownModule} from 'ng-zorro-antd/dropdown';
-import {NzMenuModule} from 'ng-zorro-antd/menu';
-import {NzTabsModule} from "ng-zorro-antd/tabs";
-import {NzRadioModule} from "ng-zorro-antd/radio";
-import {NzCollapseModule} from "ng-zorro-antd/collapse";
-import {NzFloatButtonModule} from 'ng-zorro-antd/float-button';
-import {NzSplitterModule} from 'ng-zorro-antd/splitter';
-import {NzListModule} from 'ng-zorro-antd/list';
+import { NgModule } from "@angular/core";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzSelectModule } from "ng-zorro-antd/select";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzAlertModule } from "ng-zorro-antd/alert";
+import { NzCheckboxModule } from "ng-zorro-antd/checkbox";
+import { NzCardModule } from "ng-zorro-antd/card";
+import { NzSwitchModule } from "ng-zorro-antd/switch";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { NzTabsModule } from "ng-zorro-antd/tabs";
+import { NzRadioModule } from "ng-zorro-antd/radio";
+import { NzCollapseModule } from "ng-zorro-antd/collapse";
+import { NzFloatButtonModule } from "ng-zorro-antd/float-button";
+import { NzSplitterModule } from "ng-zorro-antd/splitter";
+import { NzListModule } from "ng-zorro-antd/list";
+import { NzSpinModule } from "ng-zorro-antd/spin";
 
 @NgModule({
   exports: [
@@ -38,8 +39,7 @@ import {NzListModule} from 'ng-zorro-antd/list';
     NzCollapseModule,
     NzSplitterModule,
     NzFloatButtonModule,
-  ]
+    NzSpinModule,
+  ],
 })
-export class NgZorroAntdModule {
-
-}
+export class NgZorroAntdModule {}
