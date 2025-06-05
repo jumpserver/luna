dd7a64b21f2ce588f15415b7a5fd38efaf49fe7e
ibuler <ibuler@qq.com>
2025-06-05 15:07:25 +0800

    perf: theme color

--- src/sass/theme/main.ts
+++ src/sass/theme/main.ts
@@ -8,7 +8,8 @@ export const themeColors: any = {
 // 简化后的主题对象
 export const mainTheme = () => {
   return {
-    // 主区域背景
+    "--el-text-color-dark": "#000000d9",
+    "--el-text-color-light": "#EFEFF0",
     // 主区域背景
     "--el-main-bg-color": darken(10),
     // 主区域文本颜色
@@ -35,13 +36,13 @@ export const mainTheme = () => {
     // icon hover 时的颜色
     "--el-icon-hover-color": "#d6cbcb",
     // 侧边栏组织模块背景
-    "--el-menu-org-bg-color": darken(3),
+    "--el-menu-org-bg-color": darken(4),
     // 资产区域背景
-    "--el-assets-bg-color": darken(7),
+    "--el-assets-bg-color": darken(6),
     // 折叠面板项
     "--el-assets-fold-item-bg-color": darken(3),
     // 折叠面板展开时的背景
-    "--el-assets-extend-bg-color": darken(5),
+    "--el-assets-extend-bg-color": darken(6),
     // icon 颜色
     "--el-assets-icon-color": "#CCCCCC",
     // 滚动条的滚动块背景
@@ -50,15 +51,11 @@ export const mainTheme = () => {
     "--el-scroll-bar-track-bg-color": lighten(5),
     // 头部导航背景
     "--el-dropdown-bg-color": "#000000",
-    // 头部导航字体颜色
-    "--el-dropdown-text-color": "#EFEFF0",
     // 头部导航下拉菜单背景
     "--el-dropdown-selected-bg-color": lighten(5),
     // 头部导航下拉菜单 hover 背景
     "--el-dropdown-hover-bg-color": darken(4),
     '--el-drawer-bg-color': '#302A2A',
-    // 文本颜色
-    '--el-drawer-text-color': '#ffffff',
     // 折叠面板背景
     '--el-drawer-collapse-bg-color': '#1E1C1C'
   };
