422ba433547481460223954d6864b2c722f44331
ibuler <ibuler@qq.com>
2025-06-05 11:08:03 +0800

    perf: change org

--- src/app/elements/left-bar/organization/organization.component.ts
+++ src/app/elements/left-bar/organization/organization.component.ts
@@ -1,33 +1,34 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {CookieService} from 'ngx-cookie-service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {OrganizationService, SettingService} from '@app/services';
-import {Organization} from '@app/model';
-import {DEFAULT_ORG_ID, ROOT_ORG_ID} from '@app/globals';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { OrganizationService, SettingService } from "@app/services";
+import { Organization } from "@app/model";
+import { DEFAULT_ORG_ID, ROOT_ORG_ID } from "@app/globals";
 
 @Component({
   standalone: false,
-  selector: 'elements-organization',
-  templateUrl: 'organization.component.html',
-  styleUrls: ['organization.component.scss'],
+  selector: "elements-organization",
+  templateUrl: "organization.component.html",
+  styleUrls: ["organization.component.scss"],
 })
 export class ElementOrganizationComponent implements OnInit, OnDestroy {
-  currentOrg: Organization = {id: '', name: ''};
+  currentOrg: Organization = { id: "", name: "" };
   organizations = [];
 
-  constructor(private _cookie: CookieService,
-              public _settingSvc: SettingService,
-              private _orgSvc: OrganizationService,
-              private _router: Router,
-              private _route: ActivatedRoute
-  ) {
-  }
+  constructor(
+    private _cookie: CookieService,
+    public _settingSvc: SettingService,
+    private _orgSvc: OrganizationService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    const cookieOrgId = this._cookie.get('X-JMS-LUNA-ORG') || this._cookie.get('X-JMS-ORG');
+    const cookieOrgId =
+      this._cookie.get("X-JMS-LUNA-ORG") || this._cookie.get("X-JMS-ORG");
     this._orgSvc.orgListChange$.subscribe((res: Array<Organization>) => {
       this.organizations = res || [];
-      const cookieOrg = this.organizations.find(i => i.id === cookieOrgId);
+      const cookieOrg = this.organizations.find((i) => i.id === cookieOrgId);
       if (!cookieOrg) {
         this.currentOrg = this.getPropOrg();
         this._orgSvc.switchOrg(this.currentOrg);
@@ -42,9 +43,9 @@ export class ElementOrganizationComponent implements OnInit, OnDestroy {
   }
 
   getPropOrg() {
-    let org = this.organizations.find(item => item.id === DEFAULT_ORG_ID);
+    let org = this.organizations.find((item) => item.id === DEFAULT_ORG_ID);
     if (!org) {
-      org = this.organizations.filter(item => item.id !== ROOT_ORG_ID)[0];
+      org = this.organizations.filter((item) => item.id !== ROOT_ORG_ID)[0];
     }
     if (!org) {
       org = this.organizations[0];
@@ -52,11 +53,11 @@ export class ElementOrganizationComponent implements OnInit, OnDestroy {
     return org;
   }
 
-  changeOrg() {
-    this._orgSvc.switchOrg(this.currentOrg);
-    const query = {...this._route.snapshot.queryParams};
-    delete query['oid'];
-    delete query['_'];
+  changeOrg(org: Organization) {
+    this._orgSvc.switchOrg(org);
+    const query = { ...this._route.snapshot.queryParams };
+    delete query["oid"];
+    delete query["_"];
     this._router.navigate([], {
       relativeTo: this._route,
       queryParams: query,
