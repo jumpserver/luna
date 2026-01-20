1194e0d00d474de75108d66b9e76fdfeb94b23c6
zhaojisen <1301338853@qq.com>
2026-01-20 11:15:15 +0800

    fix: Fix the issue where automatic connection cannot be selected

--- src/app/elements/connect/connect-dialog/select-account/select-account.component.ts
+++ src/app/elements/connect/connect-dialog/select-account/select-account.component.ts
@@ -57,7 +57,7 @@ export class ElementSelectAccountComponent implements OnInit, OnDestroy {
     private _cdRef: ChangeDetectorRef
   ) {
     this.usernamePlaceholder = this._i18n.instant('Username');
-    this.rememberAuthDisabled = !this._settingSvc.globalSetting.SECURITY_LUNA_REMEMBER_AUTH;
+    this.rememberAuthDisabled = false;
   }
 
   get noSecretAccounts() {
