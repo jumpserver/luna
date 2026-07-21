0928d218d6acc32ed26bd5f75b9347fce5ba38eb
zhaojisen <1301338853@qq.com>
2026-07-21 13:43:34 +0800

    fix: chat style; login check; asset auto connect

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -9,12 +9,14 @@ import { FaceService } from '@app/services/face';
 import { TreeFilterService } from './treeFilter';
 import { OrganizationService } from './organization';
 import { AlertService } from './dialog/alert.service';
+import { LoginExpiredDialogService } from './dialog/login-expired.service';
 import { ConnectTokenService } from './connect-token/';
 import { LocalStorageService, LogService } from './share';
 import { IframeCommunicationService } from './communication';
 
 export { I18nService };
 export { AlertService };
+export { LoginExpiredDialogService };
 export { DrawerStateService };
 export { ConnectTokenService };
 export { IframeCommunicationService };
@@ -36,11 +38,12 @@ export const AllServices = [
   ViewService,
   I18nService,
   AlertService,
+  LoginExpiredDialogService,
   SettingService,
   TreeFilterService,
   DrawerStateService,
   LocalStorageService,
   OrganizationService,
   ConnectTokenService,
-  IframeCommunicationService,
+  IframeCommunicationService
 ];
